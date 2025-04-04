5746a2458a47c74f74b589f8acb59cc95a984230
TechSupport84 <trainingtech84@gmail.com>
2025-04-04 16:54:44 +0300

    added

---
+++ index.js
@@ -0,0 +1,67 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import createError from "http-errors";
+import passport from "passport";
+import session from "express-session";
+import "./config/passport.js";
+import userRouter from "./routes/user.js";
+import orderRouter from "./routes/order.js";
+import { connectDb } from "./config/db.js";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+const PORT = process.env.PORT || 3002;
+const app = express();
+app.use(cookieParser());
+app.use(
+  cors({
+    origin:process.env.CLIENT_URL, 
+    credentials: true, 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production", 
+      sameSite: "Lax",
+      maxAge: 24 * 60 * 60 * 1000, 
+    },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use("/auth", userRouter);
+app.use("/api/order", orderRouter);
+
+
+app.use((req, res, next) => {
+  next(createError(404, "Not Found!"));
+});
+
+app.use((error, req, res, next) => {
+  console.error("Error:", error.message);
+  res.status(error.status || 500).json({
+    success: false,
+    message: error.message || "Internal Server Error.",
+  });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDb();
+});
