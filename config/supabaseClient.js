c5650b00c1f118c4e42abc44e00d3492b7440b39
TechSupport84 <trainingtech84@gmail.com>
2025-05-08 18:04:36 +0300

    added

--- config/supabaseClient.js
+++ config/supabaseClient.js
@@ -1,6 +1,6 @@
 import {createClient }from "@supabase/supabase-js"
 import dotenv from "dotenv/config"
-dotenv.config()
+
 const supabase = createClient(
     process.env.SUPABASE_URL,
     process.env.SUPABASE_KEY
