ff392e8a03b919b0d80c06ba63b6cb2341a172ae
TechSupport84 <trainingtech84@gmail.com>
2025-05-08 12:28:27 +0300

    added

--- index.js
+++ index.js
@@ -17,9 +17,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin:['https://vtceaglestrans.com'], 
-    credentials: true, 
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
   })
 );
 
